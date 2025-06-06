8aacdaf7f2371540de92c87fe53d372bc497d211
otusru <otusru@mail.ru>
2025-06-06 13:52:18 +0300

    Update bootinfo.h

--- kernel/bootinfo.h
+++ kernel/bootinfo.h
@@ -1,1 +1,12 @@
+#ifndef BOOTINFO_H
+#define BOOTINFO_H
 
+#include <stdint.h>
+
+typedef struct {
+    char cmdline[256];
+    uint64_t initrd_addr;
+    uint64_t initrd_size;
+} BootInfo;
+
+#endif
