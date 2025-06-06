5ca452188dfb368e2cc17a9535df59a7f5b9ec8e
otusru <otusru@mail.ru>
2025-06-06 13:31:41 +0300

    Update bootloader.h

--- uefi_stage/bootloader.h
+++ uefi_stage/bootloader.h
@@ -18,5 +18,8 @@ EFI_STATUS ReadBootConfig(EFI_FILE_PROTOCOL* root, KernelEntry* kernels, UINTN* 
 EFI_STATUS ShowMenu(KernelEntry* kernels, UINTN kernel_count, UINTN* selection);
 EFI_STATUS LoadInitrd(CHAR16* path);
 EFI_STATUS ShowTimer(UINTN seconds);
+EFI_STATUS OpenFile(CHAR16* path, EFI_FILE_PROTOCOL** file);
+EFI_STATUS ReadFile(EFI_FILE_PROTOCOL* file, EFI_PHYSICAL_ADDRESS addr, UINTN size);
+EFI_STATUS GetFileSize(EFI_FILE_PROTOCOL* file, UINTN* size);
 
 #endif // BOOTLOADER_H
