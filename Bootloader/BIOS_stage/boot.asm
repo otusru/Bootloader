85f556577448079588a3e4c8f5f5ea16421e6e6f
otusru <otusru@mail.ru>
2025-06-06 15:29:54 +0300

    Update boot.asm

--- Bootloader/BIOS_stage/boot.asm
+++ Bootloader/BIOS_stage/boot.asm
@@ -1,53 +1,59 @@
-; bootloader.asm — Загрузочный сектор BIOS: Stage1
-; Решение: вывести сообщение и загрузить Stage2 (4 сектора, начиная с 2)
+; bios_stage/boot.asm
+; MBR-загрузчик (512 байт) для BIOS
+; Загружает второй этап загрузки (stage2.bin) с диска
 
-[BITS 16]
-[ORG 0x7C00]
+org 0x7C00                    ; адрес загрузки BIOS
+bits 16
 
 start:
-    cli
+    cli                       ; запретить прерывания
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7C00
+    mov sp, 0x7C00            ; стек
 
-    ; Показать сообщение
-    mov si, msg
+    ; Проверяем сигнатуру MBR
+    mov si, mbr_signature_msg
     call print_string
 
-    ; Загрузить 4 сектора начиная с сектора 2 (Stage2)
-    mov ah, 0x02        ; BIOS: читать секторы
-    mov al, 4           ; кол-во секторов
-    mov ch, 0
-    mov cl, 2           ; сектор 2
-    mov dh, 0
-    mov dl, 0x80        ; жёсткий диск
-    mov bx, 0x0600      ; адрес в памяти
+    ; Загружаем сектор с stage2 (сектор 2, т.к. сектор 1 - MBR)
+    mov bx, stage2_load_addr
+    mov dl, [BOOT_DRIVE]
+    mov ah, 0x02              ; функция BIOS - чтение с диска
+    mov al, 1                 ; читать 1 сектор
+    mov ch, 0                 ; цилиндр 0
+    mov cl, 2                 ; сектор 2
+    mov dh, 0                 ; головка 0
     int 0x13
     jc disk_error
 
-    ; Переход к загруженному Stage2
-    jmp 0x0000:0x0600
+    ; Переходим к stage2 (загрузчик второго этапа)
+    jmp bx
 
 disk_error:
-    mov si, error
+    mov si, disk_error_msg
     call print_string
+    hlt
     jmp $
 
 print_string:
     mov ah, 0x0E
-.print_char:
+.next_char:
     lodsb
-    test al, al
+    or al, al
     jz .done
     int 0x10
-    jmp .print_char
+    jmp .next_char
 .done:
     ret
 
-msg   db "Загрузка Otus OS Stage2...", 0
-error db "Ошибка чтения Stage2!", 0
+BOOT_DRIVE db 0             ; BIOS заполняет при старте
+
+stage2_load_addr dw 0x8000  ; адрес загрузки stage2
+
+mbr_signature_msg db "MBR loader started...", 0
+disk_error_msg db "Ошибка чтения диска", 0
 
 times 510 - ($ - $$) db 0
-dw 0xAA55
+dw 0xAA55                   ; сигнатура MBR (0x55AA в LE)
